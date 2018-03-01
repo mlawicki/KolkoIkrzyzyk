a2e4e4e37e753649f0da403e623031d3751d0bc7
Michał Ławicki <michal.lawicki.1988@gmail.com>
2018-03-01 17:30:37 +0100

    Add files via upload
    -uzupełnienie logiki gry

---
+++ KolkoIkrzyzyk/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace KolkoIkrzyzyk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.A1 = new System.Windows.Forms.Button();
+            this.A2 = new System.Windows.Forms.Button();
+            this.A3 = new System.Windows.Forms.Button();
+            this.B1 = new System.Windows.Forms.Button();
+            this.B2 = new System.Windows.Forms.Button();
+            this.B3 = new System.Windows.Forms.Button();
+            this.C1 = new System.Windows.Forms.Button();
+            this.C2 = new System.Windows.Forms.Button();
+            this.C3 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.turaLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.iloscTurLabel = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // A1
+            // 
+            this.A1.Location = new System.Drawing.Point(3, 5);
+            this.A1.Name = "A1";
+            this.A1.Size = new System.Drawing.Size(41, 34);
+            this.A1.TabIndex = 0;
+            this.A1.UseVisualStyleBackColor = true;
+            this.A1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // A2
+            // 
+            this.A2.Location = new System.Drawing.Point(50, 5);
+            this.A2.Name = "A2";
+            this.A2.Size = new System.Drawing.Size(41, 34);
+            this.A2.TabIndex = 1;
+            this.A2.UseVisualStyleBackColor = true;
+            this.A2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // A3
+            // 
+            this.A3.Location = new System.Drawing.Point(97, 5);
+            this.A3.Name = "A3";
+            this.A3.Size = new System.Drawing.Size(41, 34);
+            this.A3.TabIndex = 2;
+            this.A3.UseVisualStyleBackColor = true;
+            this.A3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // B1
+            // 
+            this.B1.Location = new System.Drawing.Point(3, 45);
+            this.B1.Name = "B1";
+            this.B1.Size = new System.Drawing.Size(41, 34);
+            this.B1.TabIndex = 3;
+            this.B1.UseVisualStyleBackColor = true;
+            this.B1.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // B2
+            // 
+            this.B2.Location = new System.Drawing.Point(50, 45);
+            this.B2.Name = "B2";
+            this.B2.Size = new System.Drawing.Size(41, 34);
+            this.B2.TabIndex = 4;
+            this.B2.UseVisualStyleBackColor = true;
+            this.B2.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // B3
+            // 
+            this.B3.Location = new System.Drawing.Point(97, 45);
+            this.B3.Name = "B3";
+            this.B3.Size = new System.Drawing.Size(41, 34);
+            this.B3.TabIndex = 5;
+            this.B3.UseVisualStyleBackColor = true;
+            this.B3.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // C1
+            // 
+            this.C1.Location = new System.Drawing.Point(3, 85);
+            this.C1.Name = "C1";
+            this.C1.Size = new System.Drawing.Size(41, 34);
+            this.C1.TabIndex = 6;
+            this.C1.UseVisualStyleBackColor = true;
+            this.C1.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // C2
+            // 
+            this.C2.Location = new System.Drawing.Point(50, 85);
+            this.C2.Name = "C2";
+            this.C2.Size = new System.Drawing.Size(41, 34);
+            this.C2.TabIndex = 7;
+            this.C2.UseVisualStyleBackColor = true;
+            this.C2.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // C3
+            // 
+            this.C3.Location = new System.Drawing.Point(97, 85);
+            this.C3.Name = "C3";
+            this.C3.Size = new System.Drawing.Size(41, 34);
+            this.C3.TabIndex = 8;
+            this.C3.UseVisualStyleBackColor = true;
+            this.C3.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.turaLabel);
+            this.groupBox1.Location = new System.Drawing.Point(144, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(48, 46);
+            this.groupBox1.TabIndex = 9;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Tura:";
+            // 
+            // turaLabel
+            // 
+            this.turaLabel.AutoSize = true;
+            this.turaLabel.Location = new System.Drawing.Point(15, 21);
+            this.turaLabel.Name = "turaLabel";
+            this.turaLabel.Size = new System.Drawing.Size(14, 13);
+            this.turaLabel.TabIndex = 0;
+            this.turaLabel.Text = "X";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.iloscTurLabel);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 5.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(146, 66);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(45, 42);
+            this.groupBox2.TabIndex = 10;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Tura nr:";
+            // 
+            // iloscTurLabel
+            // 
+            this.iloscTurLabel.AutoSize = true;
+            this.iloscTurLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
+            this.iloscTurLabel.Location = new System.Drawing.Point(16, 17);
+            this.iloscTurLabel.Name = "iloscTurLabel";
+            this.iloscTurLabel.Size = new System.Drawing.Size(13, 13);
+            this.iloscTurLabel.TabIndex = 0;
+            this.iloscTurLabel.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(192, 148);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.C3);
+            this.Controls.Add(this.C2);
+            this.Controls.Add(this.C1);
+            this.Controls.Add(this.B3);
+            this.Controls.Add(this.B2);
+            this.Controls.Add(this.B1);
+            this.Controls.Add(this.A3);
+            this.Controls.Add(this.A2);
+            this.Controls.Add(this.A1);
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(200, 175);
+            this.MinimumSize = new System.Drawing.Size(200, 175);
+            this.Name = "Form1";
+            this.Text = "Kółko i krzyżyk";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button A1;
+        private System.Windows.Forms.Button A2;
+        private System.Windows.Forms.Button A3;
+        private System.Windows.Forms.Button B1;
+        private System.Windows.Forms.Button B2;
+        private System.Windows.Forms.Button B3;
+        private System.Windows.Forms.Button C1;
+        private System.Windows.Forms.Button C2;
+        private System.Windows.Forms.Button C3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label turaLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label iloscTurLabel;
+    }
+}
+
---
+++ KolkoIkrzyzyk/Form1.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KolkoIkrzyzyk
+{
+
+
+
+    public partial class Form1 : Form
+    {
+
+        char czyjaTura = 'X'; //o-kolko,x-krzyżyk
+        int iloscTur;
+        bool wygrana = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+ 
+
+        private void Form1_Load(object sender, EventArgs e)
+        {//
+         //   Form2 frm = new Form2();
+         //   frm.ShowDialog();
+            turaLabel.Text = czyjaTura.ToString();
+            iloscTurLabel.Text = iloscTur.ToString();
+
+        }
+        void sprawdzenie()
+        {
+
+
+            if ((A1.Text == A2.Text) && (A2.Text == A3.Text)&&!A1.Enabled&&!A2.Enabled&&!A3.Enabled) { wygrana = true; MessageBox.Show("Wygrał gracz: " + A1.Text); }
+            if ((B1.Text == B2.Text) && (B2.Text == B3.Text)&&!B1.Enabled&&!B2.Enabled&&!B3.Enabled) { wygrana = true; MessageBox.Show("Wygrał gracz: " + A1.Text); }
+            if ((C1.Text == C2.Text) && (C2.Text == C3.Text) && !C1.Enabled && !C2.Enabled && !C3.Enabled) { wygrana = true; MessageBox.Show("Wygrał gracz: " + A1.Text); }
+            if ((A1.Text == B1.Text) && (B1.Text == C1.Text) && !A1.Enabled && !B1.Enabled && !C1.Enabled) { wygrana = true; MessageBox.Show("Wygrał gracz: " + A1.Text); }
+            if ((A2.Text == B2.Text) && (B2.Text == C2.Text) && !A2.Enabled && !B2.Enabled && !C2.Enabled) { wygrana = true; MessageBox.Show("Wygrał gracz: " + A1.Text); }
+            if ((A3.Text == B3.Text) && (B3.Text == C3.Text) && !A3.Enabled && !B3.Enabled && !C3.Enabled) { wygrana = true; MessageBox.Show("Wygrał gracz: " + A1.Text); }
+            if ((A1.Text == B2.Text) && (B2.Text == C3.Text) && !A1.Enabled && !B2.Enabled && !C3.Enabled) { wygrana = true; MessageBox.Show("Wygrał gracz: " + A1.Text); }
+            if ((A3.Text == B2.Text) && (B2.Text == C1.Text) && !A3.Enabled && !B2.Enabled && !C1.Enabled) { wygrana = true; MessageBox.Show("Wygrał gracz: " + A1.Text); }
+            else { remis(); }
+        }
+        void zmianaTury()
+        {
+            sprawdzenie();
+            if (czyjaTura == 'X')
+            { czyjaTura = 'O'; }
+            else 
+            { czyjaTura = 'X'; }
+            turaLabel.Text = czyjaTura.ToString();
+
+            iloscTur += 1;
+            iloscTurLabel.Text = iloscTur.ToString();
+
+
+        }
+     //   void oznaczenie()
+     //   {
+     //       this.Text = czyjaTura.ToString();
+     //       this.Enabled = false;
+     //       sprawdzenie();
+     //   }
+
+        void remis()
+        {
+            if (iloscTur == 9&&!wygrana) { MessageBox.Show("Nikt nie wygrał"); }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            A1.Text = czyjaTura.ToString();
+            zmianaTury();
+            A1.Enabled = false;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            A2.Text = czyjaTura.ToString();
+            zmianaTury();
+            A2.Enabled = false;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            A3.Text = czyjaTura.ToString();
+            zmianaTury();
+            A3.Enabled = false;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            B1.Text = czyjaTura.ToString();
+            zmianaTury();
+            B1.Enabled = false;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            B2.Text = czyjaTura.ToString();
+            zmianaTury();
+            B2.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            B3.Text = czyjaTura.ToString();
+            zmianaTury();
+            B3.Enabled = false;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            C1.Text = czyjaTura.ToString();
+            zmianaTury();
+            C1.Enabled = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            C2.Text = czyjaTura.ToString();
+            zmianaTury();
+            C2.Enabled = false;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            C3.Text = czyjaTura.ToString();
+            zmianaTury();
+            C3.Enabled = false;
+        }
+    }
+}
